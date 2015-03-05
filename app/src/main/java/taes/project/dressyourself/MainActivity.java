a90363a5ec3951272f55a14bf6a05d3a5287ce0c
Pablo Guijarro Ramirez <sky10pg@gmail.com>
2015-03-06 00:33:48 +0100

    Implementada primera version de Navigation Drawer

--- app/src/main/java/taes/project/dressyourself/MainActivity.java
+++ app/src/main/java/taes/project/dressyourself/MainActivity.java
@@ -1,30 +1,36 @@
 package taes.project.dressyourself;
 
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;    private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawer;
     private ConjuntosFragment conjuntosFragment;
+    private ListView listaDrawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         toolbar= (Toolbar) findViewById(R.id.toolbar);
         toolbar.setLogo(R.mipmap.ic_dress_your_self_circle);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listaDrawer= (ListView) findViewById(R.id.left_drawer);
 
+        listaDrawer.setAdapter(new ArrayAdapter<String>(this,R.layout.prueba_list_item,getResources().getStringArray(R.array.lst_left_drawer)));
         conjuntosFragment=new ConjuntosFragment();
 
         setSupportActionBar(toolbar);
