f6446624a2caccc3ad0610ef9e5772a613514bbb
codek <draftee@gmail.com>
2015-04-10 17:53:58 +0200

    Categorias funcionando 90%

--- app/src/main/java/taes/project/dressyourself/ParseApplication.java
+++ app/src/main/java/taes/project/dressyourself/ParseApplication.java
@@ -3,8 +3,11 @@ package taes.project.dressyourself;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.ParseACL;
+
+import taes.project.dressyourself.classes.Categoria;
 
 /**
  * Conexion con Parse.com
@@ -17,6 +20,7 @@ public class ParseApplication extends Application {
         super.onCreate();
 
         //Parse.enableLocalDatastore(this);
+        ParseObject.registerSubclass(Categoria.class);
         Parse.initialize(this, APP_ID, CLIENT_KEY);
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
--- app/src/main/java/taes/project/dressyourself/fragments/CategoriasFragment.java
+++ app/src/main/java/taes/project/dressyourself/fragments/CategoriasFragment.java
@@ -1,36 +1,106 @@
 package taes.project.dressyourself.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import taes.project.dressyourself.R;
 import taes.project.dressyourself.adapter.AdapterCategoria;
+import taes.project.dressyourself.classes.Categoria;
+import taes.project.dressyourself.utils.DividerItemDecoration;
+import taes.project.dressyourself.listeners.RecyclerItemClickListener;
 
-
-public class CategoriasFragment extends Fragment {
+public class CategoriasFragment extends Fragment implements InsertarCategoriaDialogFragment.InsertarCategoriaDialogListener {
 
     private RecyclerView listaCategorias;
-    private RecyclerView.Adapter adapter;
+    private AdapterCategoria adapter;
     private RecyclerView.LayoutManager manager;
-    private Context contexto;
-
+    private FragmentActivity context;
+    private Button insertarBtn;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.list_categorias,container,false);
+        context = getActivity();
         listaCategorias= (RecyclerView) v.findViewById(R.id.listaCategorias);
-        manager=new GridLayoutManager(getActivity(),2);
+        listaCategorias.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL_LIST));
+        listaCategorias.setHasFixedSize(true);
+        manager = new LinearLayoutManager(context);
         listaCategorias.setLayoutManager(manager);
         adapter=new AdapterCategoria();
         listaCategorias.setAdapter(adapter);
+        listaCategorias.setItemAnimator(new DefaultItemAnimator());
+        insertarBtn = (Button) v.findViewById(R.id.insertarCategoriaBtn);
+        insertarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertarCategoriaDialog();
+            }
+        });
+        listaCategorias.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), listaCategorias, new RecyclerItemClickListener.OnItemClickListener(){
+            @Override
+            public void onItemClick(View view, int position)
+            {
+                if(view.isSelected()){
+                    view.setBackgroundColor(getResources().getColor(R.color.background_material_light));
+                    view.setSelected(false);
+                }
+                Log.e("onClick", "Pos: "+position);
+            }
+            @Override
+            public void onItemLongClick(View view, final int position)
+            {
+                /*view.setSelected(true);
+                view.setBackgroundColor(getResources().getColor(R.color.primaryLight));*/
+                adapter.categorias.get(position).deleteInBackground(new DeleteCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        adapter.categorias.remove(position);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }));
         return v;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+    }
+
+    public void insertarCategoriaDialog()
+    {
+        InsertarCategoriaDialogFragment dialog = new InsertarCategoriaDialogFragment();
+        dialog.show(getFragmentManager(), "insertar_categoria_dialog");
+        dialog.setDialogListener(this);
+    }
+
+    @Override
+    public void onDialogAccept(DialogFragment dialog, Categoria c) {
+        adapter.categorias.add(c);
+        adapter.notifyDataSetChanged();
+    }
+
 }
